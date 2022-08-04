c683002eca413396a24d67d11de5201c35b7feb5
Ryan Weaver <ryan@thatsquality.com>
2022-08-04 10:49:23 -0400

    Adding some more silliness to the command

--- src/Command/GameCommand.php
+++ src/Command/GameCommand.php
@@ -25,7 +25,7 @@ class GameCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $io->text('Welcome to the game where warriors fight against each other for honor and glory!');
+        $io->text('Welcome to the game where warriors fight against each other for honor and glory... and 🍕!');
 
         $characters = $this->game->getCharactersList();
         $characterChoice = $io->choice('Select your character', $characters);
@@ -45,11 +45,11 @@ class GameCommand extends Command
         do {
             $aiCharacter = $this->selectAiCharacter();
 
-            $io->writeln('Opponent found ' . $aiCharacter->getNickname());
+            $io->writeln(sprintf('Opponent found <comment>%s</comment>', $aiCharacter->getNickname()));
 
             $fightResult = $this->game->play($player, $aiCharacter);
 
-            $this->printResult($fightResult, $io);
+            $this->printResult($fightResult, $player, $io);
 
             $answer = $io->choice('Want to keep playing?', [
                 1 => 'Fight!',
@@ -69,11 +69,25 @@ class GameCommand extends Command
         return $aiCharacter;
     }
 
-    private function printResult(FightResult $fightResult, SymfonyStyle $io)
+    private function printResult(FightResult $fightResult, Character $player, SymfonyStyle $io)
     {
+        // let's make it *feel* like a proper battle!
+        $weapons = ['🛡', '⚔️', '🏹'];
+        $io->writeln(['']);
+        $io->write('(queue epic battle sounds) ');
+        for ($i = 0; $i < $fightResult->getRounds(); $i++) {
+            $io->write($weapons[array_rand($weapons)]);
+            usleep(300000);
+        }
+        $io->writeln('');
+
         $io->writeln('------------------------------');
-        $io->writeln('Winner: ' . $fightResult->getWinner()->getNickname());
-        $io->writeln('Loser: ' . $fightResult->getLoser()->getNickname());
+        if ($fightResult->getWinner() === $player) {
+            $io->writeln('Result: <bg=green;fg=white>You WON!</>');
+        } else {
+            $io->writeln('Result: <bg=red;fg=white>You lost...</>');
+        }
+
         $io->writeln('Total Rounds: ' . $fightResult->getRounds());
         $io->writeln('Damage dealt: ' . $fightResult->getDamageDealt());
         $io->writeln('Damage received: ' . $fightResult->getDamageReceived());
