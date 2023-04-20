828c6155c5c759514130b4ce2b3c5ccd56005a29
Diego Aguiar <barret91@hotmail.com>
2023-04-20 11:18:28 -0600

    [tuts-apply-diff] Applying existing diff file for step observerdi-add-autoconfiguration

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,18 @@
 
 namespace App;
 
+use App\Observer\GameObserverInterface;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container)
+    {
+        $container->registerForAutoconfiguration(GameObserverInterface::class)
+            ->addTag('game.observer');
+    }
 }
