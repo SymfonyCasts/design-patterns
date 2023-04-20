aa44f145fa528873397910c1d37568d9b0b8d63d
Diego Aguiar <barret91@hotmail.com>
2023-04-20 11:18:26 -0600

    [tuts-apply-diff] Applying existing diff file for step builder-characterbuilder-add-setters

--- src/Builder/CharacterBuilder.php
+++ src/Builder/CharacterBuilder.php
@@ -2,6 +2,32 @@
 
 namespace App\Builder;
 
+use App\Character\Character;
+
 class CharacterBuilder
 {
+    public function setMaxHealth(int $maxHealth): self
+    {
+        // todo: implement me
+    }
+
+    public function setBaseDamage(int $baseDamage): self
+    {
+        // todo: implement me
+    }
+
+    public function setAttackType(string $attackType): self
+    {
+        // todo: implement me
+    }
+
+    public function setArmorType(string $armorType): self
+    {
+        // todo: implement me
+    }
+
+    public function buildCharacter(): Character
+    {
+        // todo: implement me
+    }
 }
