23314febb71e24ff6eeb5b11cae267c8dd395f5b
Diego Aguiar <barret91@hotmail.com>
2023-04-20 11:18:27 -0600

    [tuts-apply-diff] Applying existing diff file for step observer-xpearnedobserver-implementation

--- src/Observer/XpEarnedObserver.php
+++ src/Observer/XpEarnedObserver.php
@@ -3,11 +3,20 @@
 namespace App\Observer;
 
 use App\FightResult;
+use App\Service\XpCalculator;
 
 class XpEarnedObserver implements GameObserverInterface
 {
+    public function __construct(
+        private readonly XpCalculator $xpCalculator
+    ) {
+    }
+
     public function onFightFinished(FightResult $fightResult): void
     {
-        // TODO: Implement onFightFinished() method.
+        $winner = $fightResult->getWinner();
+        $loser = $fightResult->getLoser();
+
+        $this->xpCalculator->addXp($winner, $loser->getLevel());
     }
 }
