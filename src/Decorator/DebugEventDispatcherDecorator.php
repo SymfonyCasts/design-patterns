d138d0f574ff3148902dd33a5fa95b167a2e263f
Diego Aguiar <barret91@hotmail.com>
2023-04-20 11:18:29 -0600

    [tuts-apply-diff] Applying existing diff file for step decorator-eaeventdispatcher-delegate-all-methods

--- src/Decorator/DebugEventDispatcherDecorator.php
+++ src/Decorator/DebugEventDispatcherDecorator.php
@@ -3,11 +3,52 @@
 namespace App\Decorator;
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class DebugEventDispatcherDecorator implements EventDispatcherInterface
 {
     public function __construct(
         private readonly EventDispatcherInterface $eventDispatcher
     ) {
+    }
+
+    public function dispatch(object $event, string $eventName = null): object
+    {
+        return $this->eventDispatcher->dispatch($event, $eventName);
+    }
+
+    public function addListener(string $eventName, $listener, int $priority = 0)
+    {
+        $this->eventDispatcher->addListener($eventName, $listener, $priority);
+    }
+
+    public function addSubscriber(EventSubscriberInterface $subscriber)
+    {
+        $this->eventDispatcher->addSubscriber($subscriber);
+    }
+
+    public function removeListener(string $eventName, $listener)
+    {
+        $this->eventDispatcher->removeListener($eventName, $listener);
+    }
+
+    public function removeSubscriber(EventSubscriberInterface $subscriber)
+    {
+        $this->eventDispatcher->removeSubscriber($subscriber);
+    }
+
+    public function getListeners(string $eventName = null): array
+    {
+        return $this->eventDispatcher->getListeners($eventName);
+    }
+
+    public function getListenerPriority(string $eventName, $listener): ?int
+    {
+        return $this->eventDispatcher->getListenerPriority($eventName, $listener);
+    }
+
+    public function hasListeners(string $eventName = null): bool
+    {
+        return $this->eventDispatcher->hasListeners($eventName);
     }
 }
