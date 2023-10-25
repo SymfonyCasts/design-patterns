be5fb1c48ffb882acf534d15fbd3a028a3cbf741
Diego Aguiar <barret91@hotmail.com>
2023-10-24 20:50:51 -0600

    [tuts-apply-diff] Applying existing diff file for step observerdi-implement-compiler-pass-to-set-calls

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,10 +4,12 @@ namespace App;
 
 use App\Observer\GameObserverInterface;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+class Kernel extends BaseKernel implements CompilerPassInterface
 {
     use MicroKernelTrait;
 
@@ -15,5 +17,14 @@ class Kernel extends BaseKernel
     {
         $container->registerForAutoconfiguration(GameObserverInterface::class)
             ->addTag('game.observer');
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->findDefinition(GameApplication::class);
+        $taggedObservers = $container->findTaggedServiceIds('game.observer');
+        foreach ($taggedObservers as $id => $tags) {
+            $definition->addMethodCall('subscribe', [new Reference($id)]);
+        }
     }
 }
