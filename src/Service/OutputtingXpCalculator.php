37426287492c9eba1abed246e04a476cfe1c0d11
Diego Aguiar <barret91@hotmail.com>
2023-10-24 20:50:52 -0600

    [tuts-apply-diff] Applying existing diff file for step decorator-decorate-addxp-method

--- src/Service/OutputtingXpCalculator.php
+++ src/Service/OutputtingXpCalculator.php
@@ -2,11 +2,18 @@
 
 namespace App\Service;
 
+use App\Character\Character;
+
 class OutputtingXpCalculator implements XpCalculatorInterface
 {
     public function __construct(
         private readonly XpCalculatorInterface $innerCalculator
     )
     {
+    }
+
+    public function addXp(Character $winner, int $enemyLevel): void
+    {
+        $this->innerCalculator->addXp($winner, $enemyLevel);
     }
 }
