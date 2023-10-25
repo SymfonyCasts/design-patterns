638a510a2e580163ec6627e12220f05ce5a14870
Diego Aguiar <barret91@hotmail.com>
2023-10-24 20:50:52 -0600

    [tuts-apply-diff] Applying existing diff file for step decorator-back-up-and-show-in-raw-php

--- src/Command/GameCommand.php
+++ src/Command/GameCommand.php
@@ -5,6 +5,8 @@ namespace App\Command;
 use App\Character\Character;
 use App\FightResult;
 use App\GameApplication;
+use App\Observer\XpEarnedObserver;
+use App\Service\XpCalculator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -23,6 +25,9 @@ class GameCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $xpCalculator = new XpCalculator();
+        $this->game->subscribe(new XpEarnedObserver($xpCalculator));
+
         $io = new SymfonyStyle($input, $output);
 
         $io->text('Welcome to the game where warriors fight against each other for honor and glory... and 🍕!');
